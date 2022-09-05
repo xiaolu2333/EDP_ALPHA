18c82a994f4fb124c8dee693f3b19cf8f8054c29
dangfulin <2694977226@qq.com>
2022-09-05 14:25:33 +0800

    pref: 完成用户信息列表与详情API的序列化与反序列化功能。
    后端暂时只对接口添加登陆认证；
    前端通过用户身份字段判断是否显示接口访问链接。

--- edp_user/urls.py
+++ edp_user/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('register/', views.user_register, name='register'),
     path('logout/', views.user_logout, name='logout'),
-    path('api/v1/', views.user_serializer, name="user-serializer")
+
+    path('api/v1/', views.user_list, name="user-list"),
+    path('api/v1/<int:pk>', views.user_detail, name="user-detail"),
 ]
